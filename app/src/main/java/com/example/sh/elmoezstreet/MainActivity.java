96e82c7027105aa8fab2d9dc023c7d4112128d7a
shereen-wafa <shery.ibrahim21@gmail.com>
2016-06-07 14:43:54 +0200

    test

--- app/src/main/java/com/example/sh/elmoezstreet/MainActivity.java
+++ app/src/main/java/com/example/sh/elmoezstreet/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("ayad","test");
+        Log.e("shereeeeeeeeeeen","test");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
