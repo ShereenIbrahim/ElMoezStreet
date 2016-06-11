754e43ce0b260ef519a65f070615d600c834e6f1
ChristenaDawoud <kooky_davad2011@yahoo.com>
2016-06-11 04:40:49 -0700

    last test of camera integration

--- app/src/main/java/fragments/CameraFragment.java
+++ app/src/main/java/fragments/CameraFragment.java
@@ -1,14 +1,53 @@
 package fragments;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-
-;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import model.camera.ImageTargets;
+import com.example.sh.elmoezstreet.R;
 
 /**
  * Created by sh on 6/6/2016.
  */
 public class CameraFragment extends Fragment {
 
+    View cameraView;
+    Button startCameraActivity;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.e("cameraView", "called");
+        cameraView=inflater.inflate(R.layout.activity_camera,container,false);
+
+        return cameraView;
+
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        startCameraActivity=(Button)cameraView.findViewById(R.id.startCamera);
+
+        //when click on startCameraActivity button will open gps activity
+        startCameraActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), ImageTargets.class);
+                startActivity(intent);
+
+
+            }
+        });
+    }
 
 
 }
